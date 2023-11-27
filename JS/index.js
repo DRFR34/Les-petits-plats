0d6517428aa555e50f0276b7f68d2944b1d00583
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2023-11-27 12:03:21 +0100

    Update index.js
    deduplicate items in  dropdowns lists + A-B sorting of these items

--- JS/index.js
+++ JS/index.js
@@ -1,23 +1,22 @@
-// ! base 1 
-// ! base 1 
-// ! base 1 
-// ! base 1 
-/** Note de version
- *  OK : Affichage de toutes les cartes + remplissage des listes des menus déroulants + compte des recettes affichées 
- *  A faire :
- *      -  supprimer les doublons dans les listes dropDown
- *      -  items triés par ordre alphabetique
- *      - remontée des items cliqués en début de liste
- *      - uilisation du contenu des items cliqués dans le filtre
+
+/** Release Note
+ *  Done :  
+ *      + deduplicate items in  dropdowns lists
+ *      + A-B sorting of these items 
+ *  ToDo :
+ *      - place clicked items at the top of the list
+ *      - use of the content of the items clicked in the filter
  * 
  */
 
 
-
-
+"use strict";
+const ingredientsSet = new Set;
+const appliancesSet = new Set;
+const ustensilsSet = new Set;
 /**
- * - fetches data from a JSON file (Asynchronous)
- * - calls displayData() with the recipes data.Description placeholder
+ *  + fetches data from a JSON file (Asynchronous)
+ *  + calls displayData() with the recipes data.Description placeholder
  * 
  * @async
  * @throws {Error} - If fetching fails
@@ -28,13 +27,12 @@ async function getRecipesData() {
         const response = await fetch("./data/recipes.json");
         const allRecipes = await response.json();
         InsertRecipeCardInDom(allRecipes);
+
     } catch (error) {
         console.error("Erreur lors de la récupération des données :", error);
     }
 
 }
-
-
 
 class RecipeCard {
     constructor(recipe) {
@@ -58,40 +56,32 @@ class RecipeCard {
         </div>          
         `;
 
-        const filterAppliancesList = document.querySelector('#filterAppliancesList');
-        const filterApplianceItem = document.createElement('li');
-        filterApplianceItem.className ='filterDropdownItem';
-        filterApplianceItem.innerText = `${appliance}`;
-        filterAppliancesList.appendChild(filterApplianceItem);
+        //  deduplicate appliances for dropdown list 
+        appliancesSet.add(appliance);
 
 
-        ingredients.forEach((ingredientItem)=> {
+        ingredients.forEach((ingredientItem) => {
             const recipeIngredientsList = this.recipeCard.querySelector('.recipeIngredientsList')
-            const ingredient = ingredientItem.ingredient ;
+            const ingredient = ingredientItem.ingredient;
             const quantity = ingredientItem.quantity;
             const unit = ingredientItem.unit;
             const recipeIngredientItem = document.createElement('li');
-        recipeIngredientItem.classList.add('recipeIngredientItem');
-        recipeIngredientItem.innerHTML = `
-            <span class="ingredient"> ${ingredient}</span>
-            <span class="ingredientQuantity">${quantity ? quantity:"" } ${unit? unit : ""} </span>
-        `
-        recipeIngredientsList.appendChild(recipeIngredientItem);
+            recipeIngredientItem.classList.add('recipeIngredientItem');
+            recipeIngredientItem.innerHTML = `
+                <span class="ingredient"> ${ingredient}</span>
+                <span class="ingredientQuantity">${quantity ? quantity : ""} ${unit ? unit : ""} </span>
+            `
+            recipeIngredientsList.appendChild(recipeIngredientItem);
 
-        const filterIngredientsList = document.querySelector('#filterIngredientsList');
-        const filterIngredientItem = document.createElement('li');
-        filterIngredientItem.className ='filterDropdownItem';
-        filterIngredientItem.innerText = `${ingredient}`;
-        filterIngredientsList.appendChild(filterIngredientItem);
+            //  deduplicate ingredients for dropdown list           
+            ingredientsSet.add(ingredient);
         });
 
-        ustensils.forEach((ustensilItem)=> {
-            const filterUstensilsList = document.querySelector('#filterUstensilsList');
-            const filterustensilItem = document.createElement('li');
-            filterustensilItem.className ='filterDropdownItem';
-            filterustensilItem.innerText = `${ustensilItem}`;
-        filterUstensilsList.appendChild(filterustensilItem);   
-        });    
+        //  deduplicate ustensils for dropdown list 
+        ustensils.forEach((ustensilItem) => {
+            ustensilsSet.add(ustensilItem);
+        });
+
 
 
 
@@ -99,18 +89,78 @@ class RecipeCard {
     }
 }
 
-async function InsertRecipeCardInDom(allRecipes){
+async function InsertRecipeCardInDom(allRecipes) {
     const recipesCardsGrid = document.querySelector("#recipesCardsGrid");
-    recipesCardsGrid.innerHTML=(``)
+    recipesCardsGrid.innerHTML = (``)
     let totalRecipes = 0;
-    allRecipes.forEach((recipe) => { 
-        totalRecipes += 1 ;    
-       const recipeCard = new RecipeCard(recipe);
-       recipesCardsGrid.appendChild(recipeCard);
+
+    allRecipes.forEach((recipe) => {
+        totalRecipes += 1;
+
+        const recipeCard = new RecipeCard(recipe);
+        recipesCardsGrid.appendChild(recipeCard);
+
+        //    addIngredientsInDropdownList(ingredients)
     });
 
     const recipesNb = document.querySelector('.recipesNb');
     recipesNb.innerHTML = (`${totalRecipes}`)
+
+    addAppliancesInDropdownList();
+    addIngredientsInDropdownList();
+    addUstensilsInDropdownList();
 }
+
+
+
+
+
 getRecipesData();
+
+// == functions definitions
+
+function addAppliancesInDropdownList() {
+    const filterAppliancesList = document.querySelector('#filterAppliancesList');
+    [...appliancesSet].sort().forEach((appliance) => {
+        const filterApplianceItem = document.createElement('li');
+        filterApplianceItem.className = 'filterDropdownItem';
+        filterApplianceItem.innerText = `${appliance}`;
+        filterAppliancesList.appendChild(filterApplianceItem);
+    });
+}
+
+
+
+function addIngredientsInDropdownList() {
+    const filterIngredientsList = document.querySelector('#filterIngredientsList');
+    [...ingredientsSet].sort().forEach((ingredient) => {
+        const filterIngredientItem = document.createElement('li');
+        filterIngredientItem.className = 'filterDropdownItem';
+        filterIngredientItem.innerText = `${ingredient}`;
+        filterIngredientsList.appendChild(filterIngredientItem);
+    });
+}
+
+/**
+ *  + sort by A-B ustensilsSet, and 
+ *  + insert each ustensil in the ustensils dropdown
+ * @calledBy {fn} - fn
+ * @param {none}
+ * @returns {HTMLElements} - filterustensilItem
+ */
+function addUstensilsInDropdownList() {
+    const filterUstensilsList = document.querySelector('#filterUstensilsList');
+    /** 
+     * transform ustensilsSet in array and sort
+     * @param {String} - ustensilItem
+     */
+    [...ustensilsSet].sort().forEach((ustensilItem) => {
+        const filterustensilItem = document.createElement('li');
+        filterustensilItem.className = 'filterDropdownItem';
+        filterustensilItem.innerText = `${ustensilItem}`;
+        filterUstensilsList.appendChild(filterustensilItem)
+    });
+}
+
+
 
