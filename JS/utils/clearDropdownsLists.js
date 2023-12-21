a2335cce31f4cc0c27c3a7e3374cf3f21056440c
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2023-12-21 02:52:05 +0100

    Create clearDropdownsLists.js
    moves clearDropdownsLists(ingredientsSet, appliancesSet, ustensilsSet) in a new file

---
+++ JS/utils/clearDropdownsLists.js
@@ -0,0 +1,13 @@
+
+
+/**
+ * 
+ * 
+ *
+ * @param {Set} ingredientsSet
+ * @param {Set} appliancesSet
+ * @param {Set} ustensilsSet
+ */
+function clearDropdownsLists(ingredientsSet, appliancesSet, ustensilsSet) {
+    [ingredientsSet, appliancesSet, ustensilsSet].forEach(set => set.clear());
+}
