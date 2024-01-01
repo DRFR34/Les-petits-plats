38d5b2b40d7f2f144075b21249975b21baa1c6ea
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-01-01 22:30:51 +0100

    Add JSDoc

--- JS/utils/normalizeString.js
+++ JS/utils/normalizeString.js
@@ -1,12 +1,16 @@
-
+/**
+ * @function normalizeString
+ * @description Normalizes a string (removing accents, replacing certain characters, escaping user inputs, and converting the string to lowercase).
+ * @param {string} string - The string to be normalized.
+ * @returns {string} Returns the normalized string.
+ */
 // eslint-disable-next-line no-unused-vars
 function normalizeString(string){
-
+ 
     string = string.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    
-    // replace special caracters
     string = string.replace(/œ/g, "oe").replace(/æ/g, "ae").replace(/[']/g, " ");
-  
-    return string.toLowerCase();
 
+    //  escape user inputs
+    string = encodeURIComponent(string);
+    return string.toLowerCase();
 }
