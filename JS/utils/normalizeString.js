6d52eafc07f0878c328652cca733d2f4e2b925d9
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-01-02 00:35:30 +0100

    Update normalizeString.js
    Reorganization of the code. The new version replaces %20 with spaces, for better search performance

--- JS/utils/normalizeString.js
+++ JS/utils/normalizeString.js
@@ -3,14 +3,18 @@
  * @description Normalizes a string (removing accents, replacing certain characters, escaping user inputs, and converting the string to lowercase).
  * @param {string} string - The string to be normalized.
  * @returns {string} Returns the normalized string.
- */
+*/
 // eslint-disable-next-line no-unused-vars
 function normalizeString(string){
  
     string = string.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    string = string.replace(/œ/g, "oe").replace(/æ/g, "ae").replace(/[']/g, " ");
-
+    
     //  escape user inputs
-    string = encodeURIComponent(string);
-    return string.toLowerCase();
+    string = encodeURIComponent(string); 
+
+    string = string.replace(/œ/g, "oe").replace(/æ/g, "ae").replace(/[']/g, " ").replace(/%20/g, " ");
+
+    string = string.toLowerCase()
+
+    return string;
 }
