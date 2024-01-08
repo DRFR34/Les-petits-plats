02d42496ded4831dcc94c72b73b389631d296e09
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-01-08 13:22:47 +0100

    Add JSDocs + Delete Unnecessary comments

--- JS/index.js
+++ JS/index.js
@@ -1,46 +1,57 @@
 
-// ! base v5
-// ! base v5
-
-
 "use strict";
 
 // == Globlal Constants & Variables 
 
 // * DOM elements
 
+// eslint-disable-next-line no-unused-vars
 const mainSearchInput = document.querySelector('#mainSearchInput');
+// eslint-disable-next-line no-unused-vars
 const searchIconBox = document.querySelector('#searchIconBox');
+// eslint-disable-next-line no-unused-vars
+const mainSearchDeleteBtn= document.querySelector('#mainSearchDeleteBtn');
+// eslint-disable-next-line no-unused-vars
 const searchedWordsGrid = document.querySelector("#searchedWordsGrid");
+// eslint-disable-next-line no-unused-vars
 const ingredientsSearchInput = document.querySelector("#ingredientsSearchInput");
+// eslint-disable-next-line no-unused-vars
 const appliancesSearchInput = document.querySelector("#appliancesSearchInput");
+// eslint-disable-next-line no-unused-vars
 const ustensilsSearchInput = document.querySelector("#ustensilsSearchInput");
-
+// eslint-disable-next-line no-unused-vars
+const nothingFoundDiv = document.querySelector('#nothingFoundDiv');
 
 // * data containers
 
 const ingredientsSet = new Set;
 const appliancesSet = new Set;
 const ustensilsSet = new Set;
+// eslint-disable-next-line no-unused-vars
 const selectedTagsSet = new Set;
 
+// eslint-disable-next-line no-unused-vars
 let mainSearchValue = new String;
+// eslint-disable-next-line no-unused-vars
 let ingredientsSearchValue = new String;
+// eslint-disable-next-line no-unused-vars
 let appliancesSearchValue = new String;
+// eslint-disable-next-line no-unused-vars
 let ustensilsSearchValue = new String;
+// eslint-disable-next-line no-unused-vars
 let mainSearchResult = [];
+// eslint-disable-next-line no-unused-vars
 let currentSearch = [];
 
 
 /**
- * 
- 
- *  + fetches data from a JSON file (Asynchronous)
- *  + calls displayData() with the recipes data.Description placeholder
- * 
  * @async
- * @throws {Error} - If fetching fails
- * @returns {}
+ * @function getRecipesData
+ * @description Fetches recipe data from a JSON file. + Inserts the recipe cards into the DOM.
+ * @listens {event} fetch - Fetches the JSON data.
+ * @calls {function} InsertRecipeCardInDom 
+ * @throws {Error} Will throw an error if the fetch operation fails.
+ * @calledBy Direct init call in index.js
  */
 async function getRecipesData() {
     try {
@@ -54,10 +65,15 @@ async function getRecipesData() {
 
 }
 
-
 /**
- * @param {Array} nRecipes - arrray of objects recipes 
- * @return {Array}
+ * @async
+ * @function InsertRecipeCardInDom
+ * @description Inserts recipe cards into the DOM + updates the counter of found recipes
+ * @param {Array} nRecipes - array of objects
+ * @calls {function} RecipeCard 
+ * @calls {function} addItemsInDropdownList 
+ * @calls {function} dropdownItemsSelecting 
+ * @calledBy {function} getRecipesData
  */
 async function InsertRecipeCardInDom(nRecipes) {
     // console.log('InsertRecipeCardInDom ->nRecipes :', nRecipes);
@@ -75,7 +91,8 @@ async function InsertRecipeCardInDom(nRecipes) {
     });
 
     const recipesNb = document.querySelector('.recipesNb');
-    recipesNb.innerHTML = (`${totalRecipes}`)
+
+    recipesNb.innerHTML = (totalRecipes < 10 ? `0${totalRecipes}` : `${totalRecipes}`)
 
     addItemsInDropdownList('#filterIngredientsList', ingredientsSet);
     addItemsInDropdownList('#filterAppliancesList', appliancesSet);
@@ -88,18 +105,23 @@ getRecipesData();
 // == functions definitions
 
 /**
- *  + transforms itemsSets in arrays and sorts them
- *  + injects each item in DOM
- * @param {String} listId 
- * @param {Set} - itemsSet
+ * @function addItemsInDropdownList
+ * @description transforms itemsSets in arrays and sorts them + Adds items to a dropdown list in the DOM.
+ * @param {string} listId 
+ * @param {Set} itemsSet - The set of items to be added to the dropdown list.
+ * @calls {method} .sort 
+ * @calls {method} .forEach
+ * @calledBy {function} InsertRecipeCardInDom
  */
+
 function addItemsInDropdownList(listId, itemsSet) {
     const currentList = document.querySelector(listId);
 
-    // resets the list
+    //  resets the list
     currentList.innerHTML = (``);
 
-    [...itemsSet].sort().forEach((item) => {
+
+    [...itemsSet].sort((a, b) => a.localeCompare(b, 'fr')).forEach((item) => {
         const currentListItem = document.createElement('li');
         currentListItem.className = 'filterDropdownItem';
         currentListItem.innerText = `${item}`;
@@ -109,22 +131,19 @@ function addItemsInDropdownList(listId, itemsSet) {
 
 
 function deleteByCloseBtn(idOfInput, idOfCloseSpan, clearSearchValue) {
-    const deleteSearch = document.querySelector(idOfCloseSpan);
+    const searchDeleteBtn = document.querySelector(idOfCloseSpan);
     const searchInput = document.querySelector(idOfInput);
 
-    deleteSearch.addEventListener('click', () => {
-        console.log('deleteByCloseBtn ' + idOfInput + 'initial :', searchInput.value);
-        console.log('deleteByCloseBtn relatedSearchValue' + 'initial :', searchInput.value);
-
+    searchDeleteBtn.addEventListener('click', () => {
         searchInput.value = '';
         clearSearchValue();
-        console.log('deleteByCloseBtn relatedSearchValue' + 'final :', searchInput.value);
-
-        console.log('deleteByCloseBtn ' + idOfInput + 'final :', searchInput.value);
         refreshWithNewCriterias();
+    searchDeleteBtn.classList.remove(isVisible)
+
     });
 }
-deleteByCloseBtn('#mainSearchInput', '#deleteMainSearch', () => mainSearchValue = '');
-deleteByCloseBtn('#ingredientsSearchInput', '#deleteIngredientsSearch', () => ingredientsSearchValue = '');
-deleteByCloseBtn('#appliancesSearchInput', '#deleteAppliancesSearch', () => appliancesSearchValue = '');
-deleteByCloseBtn('#ustensilsSearchInput', '#deleteUstensilsSearch', () => ustensilsSearchValue = '');
+
+deleteByCloseBtn('#mainSearchInput', '#mainSearchDeleteBtn', () => mainSearchValue = '');
+deleteByCloseBtn('#ingredientsSearchInput', '#ingredientsSearchDeleteBtn', () => ingredientsSearchValue = '');
+deleteByCloseBtn('#appliancesSearchInput', '#appliancesSearchDeleteBtn', () => appliancesSearchValue = '');
+deleteByCloseBtn('#ustensilsSearchInput', '#ustensilsSearchDeleteBtn', () => ustensilsSearchValue = '');
